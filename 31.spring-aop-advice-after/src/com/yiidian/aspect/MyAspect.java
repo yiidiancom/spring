c052b40fe291e1a133682654527ed6edde3e1244
eric <1014671449@qq.com>
2017-11-14 07:34:04 +0800

    qq

--- 31.spring-aop-advice-after/src/com/yiidian/aspect/MyAspect.java
+++ 31.spring-aop-advice-after/src/com/yiidian/aspect/MyAspect.java
@@ -16,7 +16,7 @@ public class MyAspect {
 	 * JoinPoint:代表当前拦截的方法对象，使用该对象可以获取拦截方法的信息（例如：类名，方法名，方法参数等）
 	 */
 	public void after(JoinPoint jp){
-		System.out.println("执行了后置通知");
+		System.out.println("执行了最终通知");
 		System.out.println("代理对象类型："+jp.getThis().getClass());
 		System.out.println("拦截的方法名称："+jp.getSignature().getName());
 		System.out.println("拦截方法的参数列表："+Arrays.asList(jp.getArgs()));
